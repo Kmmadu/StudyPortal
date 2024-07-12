0e6f6fc94a85e4cecf17a5e9d6d7e79f2c946e85
kmmadu <mmadubugwukingsley@gmail.com>
2024-07-12 22:53:46 +0100

    Added the why chose us and FAQ section

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,13 @@
 function toggleMenu() {
     const headerRight = document.querySelector('.header-right');
-    headerRight.classList.toggle('active');
+    headerRight.style.display = headerRight.style.display === 'flex' ? 'none' : 'flex';
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const mediaQuery = window.matchMedia('(max-width: 768px)');
+    const headerRight = document.querySelector('.header-right');
+    headerRight.style.display = mediaQuery.matches ? 'none' : 'flex';
+    mediaQuery.addListener(function(e) {
+        headerRight.style.display = e.matches ? 'none' : 'flex';
+    });
+});
