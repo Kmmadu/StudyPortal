b275eb495f7f224a11782de277de151c9785ca29
kmmadu <mmadubugwukingsley@gmail.com>
2024-07-16 21:25:27 +0100

    Editing the nav_bar

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -11,7 +11,3 @@ document.addEventListener('DOMContentLoaded', function() {
         headerRight.style.display = e.matches ? 'none' : 'flex';
     });
 });
-function toggleMenu() {
-    var headerRight = document.querySelector('.header-right');
-    headerRight.classList.toggle('active');
-}
